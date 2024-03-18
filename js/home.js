6dcf0ffe8c78da92a90136a936127a93679f4eb8
Marvel Collin <marvelcollin7@gmail.com>
2024-03-18 13:37:26 +0700

    fix: background-image issue

--- js/home.js
+++ js/home.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch('components/navbar.html')
         .then(response => response.text())
         .then(html => {
-            // Insert the HTML content into the specified element
             document.getElementById('includedContent').innerHTML = html;
         })
         .catch(error => console.error('Error fetching included content:', error));
