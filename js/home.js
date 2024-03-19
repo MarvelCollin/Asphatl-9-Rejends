46d9141c2f9690a792249630af7aaa94d6069f57
Marvel Collin <marvelcollin7@gmail.com>
2024-03-19 10:45:29 +0700

    feat: add moving jumbotron

--- js/home.js
+++ js/home.js
@@ -6,3 +6,14 @@ document.addEventListener("DOMContentLoaded", function() {
         })
         .catch(error => console.error('Error fetching included content:', error));
 });
+
+document.addEventListener("mousemove", function(e) {
+    const jumbotron = document.querySelector("#jumbotron");
+    const container = document.querySelector(".moving");
+    const mouseX = e.pageX;
+    const mouseY = e.pageY;
+    const jumbotronRect = jumbotron.getBoundingClientRect();
+    const offsetX = (mouseX - (jumbotronRect.left + jumbotronRect.width / 2)) / 10;
+    const offsetY = (mouseY - (jumbotronRect.top + jumbotronRect.height / 2)) / 10;
+    container.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+});
