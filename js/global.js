3dde0d00f1ad31f93636155c611bf8aab15dd763
Marvel Collin <marvelcollin7@gmail.com>
2024-03-20 00:33:16 +0700

    feat: add footer

--- js/global.js
+++ js/global.js
@@ -2,7 +2,8 @@ function extend(path, targetId, page) {
   fetch(path)
     .then((response) => response.text())
     .then((html) => {
-      document.getElementById(targetId).innerHTML = html;
-      document.getElementById(page).classList.add("active");
+      const $ = document;
+      $.getElementById(targetId).innerHTML = html;
+      $.getElementById(page).classList.add("active");
     });
 }
