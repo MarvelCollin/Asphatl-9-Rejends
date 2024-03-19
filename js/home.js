1ac2126fbcac4132d6d2a3f78368a534b2dcc50a
Marvel Collin <marvelcollin7@gmail.com>
2024-03-19 11:02:18 +0700

    fix: extend useless func

--- js/home.js
+++ js/home.js
@@ -1,12 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('components/navbar.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('includedContent').innerHTML = html;
-        })
-        .catch(error => console.error('Error fetching included content:', error));
-});
-
 document.addEventListener("mousemove", function(e) {
     const jumbotron = document.querySelector("#jumbotron");
     const container = document.querySelector(".moving");
