a8121b3ff04c2fb3efd3931eb8437e8bff5dd4ca
Marvel Collin <marvelcollin7@gmail.com>
2024-03-20 15:44:27 +0700

    feat: adding json cars data

---
+++ js/cars.js
@@ -0,0 +1,91 @@
+const cars = // painful :< cuz manually adding json one by one huhhuuu. js vanilla doesn't have raed-folder function
+    {
+        "classS": [
+            {
+                "imgUrl": "../assets/cars/S/Ferrari-FXX-K.webp",
+                "title": "Ferrari FXX K",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+            },
+            {
+                "imgUrl": "../assets/cars/S/Icona-Vulcano-Titanium.webp",
+                "title": "Icona Vulcano Titanium",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+            },
+            {
+                "imgUrl": "../assets/cars/S/Lamborghini-Centenario.webp",
+                "title": "Lamborghini Centenario",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+            },
+            {
+                "imgUrl": "../assets/cars/S/Trion-Nemesis.webp",
+                "title": "Trion Nemesis",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+            }
+        ],
+        "classB": [
+            {
+                "imgUrl": "../assets/cars/B/Aston-Martin-DB11.webp",
+                "title": "Aston Martin DB11",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+            },
+            {
+                "imgUrl": "../assets/cars/B/Exotic-Rides-W70.webp",
+                "title": "Exotic Rides W70",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+            },
+            {
+                "imgUrl": "../assets/cars/B/Jaguar-F-Type-SVR.webp",
+                "title": "Jaguar F-Type SVR",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+            },
+            {
+                "imgUrl": "../assets/cars/B/Porsche-911-GTS-Coupe.webp",
+                "title": "Porsche 911 GTS Coupe",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+            }
+        ],
+        "classC": [
+            {
+                "imgUrl": "../assets/cars/C/BMW-30-CSL-Hommage.webp",
+                "title": "BMW 30 CSL Hommage",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+            },
+            {
+                "imgUrl": "../assets/cars/C/Dodge-Challenger-SRT8.webp",
+                "title": "Dodge Challenger SRT8",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+            },
+            {
+                "imgUrl": "../assets/cars/C/Mercedes-Benz-Amg-GT-S.webp",
+                "title": "Mercedes Benz Amg GT S",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+            },
+            {
+                "imgUrl": "../assets/cars/C/Lotus-Evora-Sport-410.webp",
+                "title": "Lotus Evora Sport 410",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+            }
+        ],
+        "classD": [
+            {
+                "imgUrl": "../assets/cars/D/Lotus-Emira.webp",
+                "title": "Lotus Emira",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+            },
+            {
+                "imgUrl": "../assets/cars/D/Mitsubishi-Lancer-Evolution.webp",
+                "title": "Mitsubishi Lancer Evolution",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+            },
+            {
+                "imgUrl": "../assets/cars/D/Porsche-718-Cayman.webp",
+                "title": "Porsche 718 Cayman",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio"
+            },
+            {
+                "imgUrl": "../assets/cars/D/Renault-Dezir.webp",
+                "title": "Renault Dezir",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+            }
+        ]
+    }
