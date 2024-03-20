5e43d83305f7fa4bc66fb13ab6e03b3c684f5205
Marvel Collin <marvelcollin7@gmail.com>
2024-03-20 14:59:09 +0700

    feat: i think my brain is commited to suicide cuz of js vanilla

--- js/products.js
+++ js/products.js
@@ -1,8 +1,59 @@
+const classS = [
+    {
+        imgUrl: "../assets/cars/A/",
+        title: "Aston Martin Vulcan",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+    },
+    {
+        imgUrl: "../assets/cars/A/Ferrari-J50.webp",
+        title: "Ferrari J50",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+    },
+    {
+        imgUrl: "../assets/cars/A/Nissan-GT-R-Nismo.webp",
+        title: "Nissan GT R Nismo",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+    },
+    {
+        imgUrl: "../assets/cars/A/Nio-EP9.webp",
+        title: "Nio EP9",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+    }
+];
+
+
+const classA = [
+    {
+        imgUrl: "../assets/cars/A/Aston-Martin-Vulcan.webp",
+        title: "Aston Martin Vulcan",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+    },
+    {
+        imgUrl: "../assets/cars/A/Ferrari-J50.webp",
+        title: "Ferrari J50",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+    },
+    {
+        imgUrl: "../assets/cars/A/Nissan-GT-R-Nismo.webp",
+        title: "Nissan GT R Nismo",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+    },
+    {
+        imgUrl: "../assets/cars/A/Nio-EP9.webp",
+        title: "Nio EP9",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolor, numquam omnis possimus eveniet amet saepe molestias, quod nihil unde libero officiis illum odio enim! Doloremque accusantium et sequi sit."
+    }
+];
+
+
+
+
+
 const cards = document.querySelectorAll(".card");
 const prevButton = document.querySelector(".button-section .btn:first-child");
 const nextButton = document.querySelector(".button-section .btn:last-child");
 
-let currentIndex = 0;
+let currentIndex = 1;
 
 function showCurrentCard() {
   cards.forEach((card, index) => {
