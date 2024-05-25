6f583740ae1848c32b609af08967757494f5ecfe
Marvel Collin <marvelcollin7@gmail.com>
2024-05-25 22:24:13 +0700

    feat: add more animation

--- js/home.js
+++ js/home.js
@@ -4,7 +4,7 @@ document.addEventListener("mousemove", function(e) {
     const mouseX = e.pageX;
     const mouseY = e.pageY;
     const jumbotronRect = jumbotron.getBoundingClientRect();
-    const offsetX = (mouseX - (jumbotronRect.left + jumbotronRect.width / 2)) / 10;
-    const offsetY = (mouseY - (jumbotronRect.top + jumbotronRect.height / 2)) / 10;
+    const offsetX = (mouseX - (jumbotronRect.left + jumbotronRect.width / 2)) / 5;
+    const offsetY = (mouseY - (jumbotronRect.top + jumbotronRect.height / 2)) / 5;
     container.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
 });
